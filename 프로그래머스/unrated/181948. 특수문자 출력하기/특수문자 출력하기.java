2787e6b1f606ba59a0832e245bb5c101edc94d3e
ighwang <126748548+ighwang@users.noreply.github.com>
2023-11-27 22:05:28 +0900

    [level unrated] Title: 특수문자 출력하기, Time: 144.57 ms, Memory: 63.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String letters="!@#$%^&*(\\'\"<>?:;";
+        
+        System.out.println(letters);
+    }
+}
