6d953de5e243fcc4d99f65abfb9bfc218abff734
ighwang <126748548+ighwang@users.noreply.github.com>
2023-11-29 01:08:25 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.03 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+        
+        if(a!=b && b!=c && c!=a){
+            answer=a+b+c;
+        }else if(a==b && b==c && c==a){
+            double temp=(a+b+c)*(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2))*(Math.pow(a,3)+Math.pow(b,3)+Math.pow(c,3));
+            answer=(int)temp;
+        }else{
+            double temp=(a+b+c)*(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2));
+            answer=(int)temp;
+        }
+        
+        return answer;
+    }
+}
